08f6e38e23430849688bdfd7c9cd93cc59ad9b49
Daimonke <Daimonas98@gmail.com>
2022-05-24 10:04:02 +0300

    Added: api

---
+++ routes/api/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+import con from "../../dbCon.js";
+
+const router = express.Router()
+
+router.get('/:id?', async (req, res) => {
+    try {
+        const [data] = await con.query(`
+        SELECT * FROM user
+        ${req.params.id ? `WHERE id = ?` : ''}
+        `, [req.params.id])
+        res.json(data)
+
+    } catch (err) {
+        res.json({ err: err });
+    }
+});
+
+export default router
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import con from '../dbCon.js'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
-        res.render('index', { css: 'index.css' })
+        const [data] = await con.query(`
+        SELECT blog.*, user.name FROM blog
+        JOIN user ON blog.author_id = user.id
+        `)
+        res.render('index', { css: 'index.css', data: data })
 
     } catch (err) {
         res.json({ err: err });
