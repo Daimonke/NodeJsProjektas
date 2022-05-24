3ece6e903813a871ae51b2fd9ecc21c891a82205
Daimonke <Daimonas98@gmail.com>
2022-05-24 10:05:31 +0300

    Added: register

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import index from './routes/index.js'
+import register from './routes/register.js'
 import users from './routes/api/users.js'
 import blog from './routes/api/blog.js'
 import path from 'path'
@@ -19,6 +20,7 @@ app.use(express.urlencoded( {extended:false} ));
 
 app.use('/api/users', users)
 app.use('/api/blog', blog)
+app.use('/register', register)
 app.use('/', index)
 
 app.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`))
